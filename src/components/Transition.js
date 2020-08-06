a16200fe04d828400df255a6463a11dbe74f6a76
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-06 23:36:09 +0100

    adding a few css class to transition between states

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,48 +1,36 @@
-import React, { Component } from 'react';
-import { Transition } from 'react-transition-group';
+import React, { Component } from "react";
+import { Transition } from "react-transition-group";
 
-import '../css/App.css';
+import "../css/App.css";
 
-class TransitionComp extends Component{
+class TransitionComp extends Component {
+  state = {
+    show: false,
+  };
 
-    state = {
-        show: false,
-    }
+  showDiv = () => {
+    this.setState({
+      show: !this.state.show ? true : false,
+    });
+  };
 
-    showDiv = () => {
-        this.setState({
-            show: !this.state.show ? true : false
-        })
-    }
-
-    render(){
-        return(
-            <>
-            <Transition
-              in={this.state.show}
-              timeout={1000}
-              mountOnEnter
-            >
-                { state => 
-                <div style={{
-                    background: 'red',
-                    height: '100px',
-                    transition: 'all 1s ease',
-                    opacity: state === 'exited' || state === 'exiting' ? 0 : 1
-                }}>
-                    {state}
-                    </div>
-                }
-            </Transition>
-
-            <div className="showDiv" onClick={this.showDiv}>
-                show/hide
+  render() {
+    return (
+      <>
+        <Transition in={this.state.show} timeout={1000}>
+          {(state) => (
+            <div className={`square square-${state}`}>
+              {`square square-${state}`}
             </div>
-            </>
+          )}
+        </Transition>
 
-        )
-    }
+        <div className="showDiv" onClick={this.showDiv}>
+          show/hide
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default TransitionComp;
