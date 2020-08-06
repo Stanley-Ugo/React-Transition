12dde6e02642d71bea1a39cea49f050482e50404
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-06 23:55:21 +0100

    adding state & button to CSStransition Component

--- src/components/CSStransition.js
+++ src/components/CSStransition.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import '../css/App.css';
+import "../css/App.css";
 
-class Fade extends Component{
-    render(){
-        return(
-            <div>
-                CSS TRANSITIONS
-            </div>
-        )
-    }
+class Fade extends Component {
+  state = {
+    show: false,
+  };
+
+  showDiv = () => {
+    this.setState({
+      show: !this.state.show ? true : false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="showDiv" onClick={this.showDiv}>
+          show/hide
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default Fade;
