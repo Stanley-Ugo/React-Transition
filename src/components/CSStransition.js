647c033bdaace8c9f4809d9cf397d5223a0f6292
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-07 00:21:11 +0100

    adding a css classes to the state of the CSStransition component

--- src/components/CSStransition.js
+++ src/components/CSStransition.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { CSSTransition } from 'react-transition-group';
 
 import "../css/App.css";
 
@@ -16,6 +17,13 @@ class Fade extends Component {
   render() {
     return (
       <>
+        <CSSTransition
+          in={this.state.show}
+          timeout={500}
+          classNames="square"
+        >
+            <div className={`square ${this.state.show}`}> Hello </div>
+        </CSSTransition>
         <div className="showDiv" onClick={this.showDiv}>
           show/hide
         </div>
